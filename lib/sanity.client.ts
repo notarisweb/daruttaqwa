6df791dd4e4f345abc6a9b50ac13c0e5f67193e8
Azhar Hanin <aris.suharyanto79@gmail.com>
2026-05-03 06:02:22 +0700

    sanity ok

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "v9y48nrd", // Ganti dengan Project ID Sanity Anda
+  projectId: "u9n0ne1d", // Ganti dengan Project ID Sanity Anda
   dataset: "production",        // Biasanya "production"
-  apiVersion: "2024-01-01",     // Gunakan tanggal hari ini atau versi terbaru
+  apiVersion: "2026-05-03",     // Gunakan tanggal hari ini atau versi terbaru
   useCdn: false,                // Set ke false agar data selalu terbaru (fresh)
 });
